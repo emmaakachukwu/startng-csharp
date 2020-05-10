b197d9760a25b5e38dc8415ca6704ab1630b2e93
emmanuel akachukwu <emmanuelakachukwu1@gmail.com>
2020-05-10 09:11:56 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using task4.Models;
+
+namespace task4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstNumber, string secondNumber)
+        {
+            int firstNum;
+            int secondNum;
+            int32.TryParse firstSqrt;
+            int32.TryParse secondSqrt;
+
+            firstNum = int.Parse(firstNumber);
+            secondNum = int.Parse(secondNumber);
+
+            firstSqrt = Math.Sqrt(firstNum);
+            secondSqrt = Math.Sqrt(secondNum);
+
+            if ( firsSqrt > secondSqrt ) {
+                string result = "The number "+firstNum+" with square root "+firstSqrt+" has a higher square root than the number "+secondNum+" with square root "+secondSqrt;
+            } else if ( firsSqrt > secondSqrt ) {
+                result = "The number "+secondNum+" with square root "+secondSqrt+" has a higher square root than the number "+firstNum+" with square root "+firstSqrt;
+            } else if ( firstSqrt == secondSqrt ) {
+                result = "The number "+firstNum+" with square root "+firstSqrt+" has an equal square root with the number "+secondNum;
+            }
+
+            ViewBag.Result = result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,65 +1,26 @@
-﻿using System.Security.Cryptography;
-using System.Data;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace csharp_task3
+namespace task4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string level;
-            int guess_limit;
-            int max_number;
-            int secret_number;
-            bool game_over = false;
-
-            Random rnd = new Random();
-            while(true){
-                Console.Write("Choose your level (easy, medium or hard): ");
-                level = Console.ReadLine();
-
-                if ( level == "easy" ) {
-                    guess_limit = 6;
-                    max_number = 10;
-                    secret_number =  rnd.Next(1, max_number);
-                    break;
-                } else if ( level == "medium" ) {
-                    guess_limit = 4;
-                    max_number = 20;
-                    secret_number = rnd.Next(1, max_number);;
-                    break;
-                } else if ( level == "hard" ) {
-                    guess_limit = 3;
-                    max_number = 50;
-                    secret_number = rnd.Next(1, max_number);;
-                    break;
-                } else {
-                    continue;
-                }
-            }
-
-            int guess_count = 1;
-            while ( guess_count <= guess_limit ) {
-                Console.Write("Guess the secret number from 1 - " + max_number + ": ");
-                int guess = int.Parse(Console.ReadLine());
-                if ( guess == secret_number ) {
-                    Console.WriteLine("You got it right");
-                    break;
-                } else {
-                    int remaining_guesses = guess_limit - guess_count;
-                    if ( remaining_guesses > 0 ) {
-                        Console.WriteLine("That was wrong. You have " + remaining_guesses + " guesses left");
-                    } else {
-                        game_over = true;
-                    }
-                    guess_count ++;
-                }
-            }
-            
-            if ( game_over ) {
-                Console.WriteLine("Game Over!");
-            }
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
